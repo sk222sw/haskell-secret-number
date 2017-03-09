9de9e534e674a9d4b2c1965e7723ce9845ea9993
Sonny <sonny@beanloop.se>
2017-03-09 21:49:48 +0100

    dont throw error when non-integers are entered

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,37 @@
+{-# Language OverloadedStrings #-}
+
 module Main where
 
 import System.Random
 import System.Console.ANSI (clearScreen)
+import Text.Read (readMaybe)
 
 data GameState = GameState { numberToGuess::Integer, numTries::Integer}
                    deriving (Show)
 
 clearAndReturn something = do
   clearScreen
-  putStrLn "That was correct!!!"
+  putStrLn "That was correct!"
   return something
+
+safeRead :: IO Integer
+safeRead = do
+  s <- getLine
+  case readMaybe s :: Maybe Integer of
+    Nothing -> do
+      putStrLn "Only numbers"
+      safeRead
+    Just s -> return s
 
 gameLoop :: GameState -> IO GameState
 gameLoop gs = do
   print $ numberToGuess gs
   putStrLn "Enter a number:"
-  s <- getLine
-  let num = read s :: Integer
+  s <- safeRead
+  let num = s :: Integer
   if num == numberToGuess gs then
                              clearScreen >> return gs
-  else gameLoop $ GameState (numberToGuess gs) ((numTries gs) + 1)
+  else gameLoop $ GameState (numberToGuess gs) (numTries gs + 1)
 
 main = do
   pTime <- randomRIO(1,4)
