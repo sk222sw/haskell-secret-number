8f7a69b8f781ddfa3262e1c25ade61135554bf95
Sonny <sonny@beanloop.se>
2017-03-10 08:18:08 +0100

    text fixes

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,4 @@
-{-# Language OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
@@ -6,35 +6,49 @@ import System.Random
 import System.Console.ANSI (clearScreen)
 import Text.Read (readMaybe)
 
-data GameState = GameState { numberToGuess::Integer, numTries::Integer}
-                   deriving (Show)
+data GameState = GameState
+    { numberToGuess::Integer
+    , numTries     ::Integer
+    } deriving (Show)
 
-clearAndReturn something = do
-  clearScreen
-  putStrLn "That was correct!"
-  return something
+-- type Guess = Integer
+
+minGuess = 1
+maxGuess = 4
+
+showCorrectText :: GameState -> IO ()
+showCorrectText gs = clearScreen >> putStrLn "That was correct!"
+
+howToPlayText :: Integer -> Integer -> String
+howToPlayText min max = "Enter a number between " ++ show min ++ " and " ++ show max
 
 safeRead :: IO Integer
 safeRead = do
-  s <- getLine
-  case readMaybe s :: Maybe Integer of
-    Nothing -> do
-      putStrLn "Only numbers"
-      safeRead
-    Just s -> return s
+    s <- getLine
+    case readMaybe s :: Maybe Integer of
+        Nothing -> putStrLn "Only numbers" >> safeRead
+        Just s  -> return s
 
-gameLoop :: GameState -> IO GameState
+tooLowOrTooHighText :: Integer -> Integer -> String
+tooLowOrTooHighText answer guess =
+    if answer > guess 
+        then "Too low" 
+        else "Too high"
+
+gameLoop :: GameState -> IO ()
 gameLoop gs = do
-  print $ numberToGuess gs
-  putStrLn "Enter a number:"
-  s <- safeRead
-  let num = s :: Integer
-  if num == numberToGuess gs then
-                             clearScreen >> return gs
-  else gameLoop $ GameState (numberToGuess gs) (numTries gs + 1)
+    putStrLn "Enter a number:"
+    s <- safeRead
+    let num = s :: Integer
+    if num == numberToGuess gs
+        then showCorrectText gs
+        else do
+            putStrLn $ tooLowOrTooHighText (numberToGuess gs) s
+            gameLoop $ GameState (numberToGuess gs) (numTries gs + 1)
 
 main = do
-  pTime <- randomRIO(1,4)
-  let gameState = GameState pTime 1
-  print "Guess a number between 1 and 4"
-  gameLoop gameState
+    pTime <- randomRIO (minGuess, maxGuess)
+    let gameState = GameState pTime 1
+    clearScreen
+    print $ howToPlayText minGuess maxGuess
+    gameLoop gameState
