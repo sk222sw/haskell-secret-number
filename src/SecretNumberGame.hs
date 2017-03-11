f95f696b6ac0ef807d09055fb95345d78b6a949c
Sonny <sonny@beanloop.se>
2017-03-11 13:37:00 +0100

    calculate new range

--- src/SecretNumberGame.hs
+++ src/SecretNumberGame.hs
@@ -1,29 +1,33 @@
-module SecretNumberGame
-    ( initGame
-    , tooLowOrTooHighText
-    , correctGuessText
-    , validGuess
-    , GameState (GameState)
-    ) where
+module SecretNumberGame where
 
 import System.Random
 import System.Console.ANSI (clearScreen)
 import Text.Read (readMaybe)
 
-data GameState = GameState
-    { numberToGuess :: Integer
+type GuessRange = (Integer, Integer)
+type Answer     = Integer
+type Guess      = Integer
+
+data GameState      =  GameState
+    { numberToGuess :: Answer
     , numTries      :: Integer
+    , guessRange    :: GuessRange
     } deriving (Show)
 
 minGuess = 1
 maxGuess = 100
 
+calculateNewRange :: GuessRange -> Answer -> Guess -> GuessRange
+calculateNewRange range answer guess
+    | guess < answer = (guess, snd range)
+    | otherwise = (fst range, guess)
+
 correctGuessText :: GameState -> String
 correctGuessText gs = 
-    show (numberToGuess gs) ++ 
-    " is correct! It took you " ++ 
-    show (numTries gs) ++ 
-    tryOrTries (numTries gs)
+       show (numberToGuess gs) 
+    ++ " is correct! It took you " 
+    ++ show (numTries gs)
+    ++ tryOrTries (numTries gs)
         where tryOrTries n = if n == 1 then " try." else " tries."
 
 validGuess :: String -> Bool
@@ -32,12 +36,28 @@ validGuess s =
         Nothing -> False
         Just s  -> True
 
-safeRead :: IO Integer
-safeRead = do
+-- not using >= or <= because for some reason it didnt pass the tests. 
+guessIsInRange :: GuessRange -> Integer -> Bool
+guessIsInRange range guess
+    | guess <  fst range = False
+    | guess >  snd range = False
+    | guess == fst range = False
+    | guess == snd range = False
+    | otherwise          = True
+
+-- TODO: Dont nest ifs
+safeRead :: GameState -> IO Integer
+safeRead gs = do
     s <- getLine
     if validGuess s
-        then return $ read s
-        else putStrLn "Only integers" >> safeRead
+        then
+            if guessIsInRange (guessRange gs) (read s)
+                then return $ read s
+                else safeRead gs
+        else putStrLn "Only integers" >> safeRead gs
+
+tooLow :: Integer -> Integer -> Bool
+tooLow guess answer = guess < answer
 
 tooLowOrTooHighText :: Integer -> Integer -> String
 tooLowOrTooHighText answer guess =
@@ -49,18 +69,18 @@ gameLoop :: GameState -> IO ()
 gameLoop gs = do
     print $ numberToGuess gs
     putStrLn "Enter a number:"
-    s <- safeRead
+    s <- safeRead gs
     let num = s :: Integer
     if num == numberToGuess gs
         then putStrLn $ correctGuessText gs
         else do
             putStrLn $ tooLowOrTooHighText (numberToGuess gs) s
-            gameLoop $ GameState (numberToGuess gs) (numTries gs + 1)
+            gameLoop $ GameState (numberToGuess gs) (numTries gs + 1) (minGuess, maxGuess)
 
 initGame :: IO ()
 initGame = do
     answer <- randomRIO (minGuess, maxGuess)
-    let gameState = GameState answer 1
+    let gameState = GameState answer 1 (minGuess, maxGuess)
     clearScreen
     putStrLn $ howToPlayText minGuess maxGuess
     gameLoop gameState
--- test/Spec.hs
+++ test/Spec.hs
@@ -12,13 +12,28 @@ spec = do
            tooLowOrTooHighText 2 3 `shouldBe` "Too high"
     describe "correctGuessText" $
         do it "should show a text with the correct answer and number of tries" $
-               correctGuessText (GameState 2 2) `shouldBe`
+               correctGuessText (GameState 2 2 (1, 100)) `shouldBe`
                "2 is correct! It took you 2 tries."
            it "should only pluralize if not provded a 1" $
-               correctGuessText (GameState 2 2) `shouldBe`
+               correctGuessText (GameState 2 2 (1, 100)) `shouldBe`
                "2 is correct! It took you 2 tries."
     describe "validGuess" $
         it "checks if a string is an integer" $ do
-          validGuess "hej" `shouldBe` False  
-          validGuess "1" `shouldBe` True  
-          validGuess "1.23" `shouldBe` False  
+            validGuess "hej" `shouldBe` False  
+            validGuess "1" `shouldBe` True  
+            validGuess "1.23" `shouldBe` False 
+    describe "guessIsInRange" $
+        it "checks if a value is within previous guesses" $ do
+            guessIsInRange (23, 48) 28  `shouldBe` True
+            guessIsInRange (23, 48) 1   `shouldBe` False
+            guessIsInRange (23, 48) 99  `shouldBe` False
+            guessIsInRange (23, 48) 23  `shouldBe` False
+            guessIsInRange (23, 48) 48  `shouldBe` False
+    describe "calculateNewRange" $ do
+        it "should make the lower range the guess if guess is too low" $ do
+            let oldRange = (34,93)
+            let answer = 55
+            let guess = 40
+            calculateNewRange oldRange answer guess `shouldBe` (40,93)
+        it "should make the lower range the guess if guess is too low" $
+            calculateNewRange (34,93) 55 70 `shouldBe` (34,70)
